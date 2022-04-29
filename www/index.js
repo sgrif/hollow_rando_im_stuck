cc0e47299d970fc8e97bfda22d65f14a853b7d84
Sage Griffin <sage@sagetheprogrammer.com>
2022-04-29 08:50:15 -0600

    Persist file handles accross visits, display whether we can autoreload
    The FileSystemHandle API is only available in Chrome, so the behavior
    will different in those browsers vs Firefox. We should display that
    information to the user, and if we have access to a handle, stick it in
    IndexedDB so they don't have to open the filepicker again next time they
    visit. (This will still result in a prompt to grant access, but that's
    at least easier to click).
    
    It'd be nice if we could show the full path, but that doesn't appear to
    be possible.

--- www/index.js
+++ www/index.js
@@ -1,46 +1,102 @@
 import * as wasm from "hollow-rando-im-stuck";
 import { fileOpen } from "browser-fs-access";
+import { get, set } from "idb-keyval";
 
 let select_files = document.querySelector("#select_files");
 let run_button = document.querySelector("#run");
 let show_items_cb = document.querySelector("#show_items");
 let show_unlocked_locations_cb = document.querySelector("#show_unlocked_locations");
 let output = document.querySelector("#output");
+let raw_spoiler_status = document.querySelector("#raw_spoiler_status");
+let tracker_log_status = document.querySelector("#tracker_log_status");
 
-var raw_spoiler = null;
-var tracker_log = null;
-
-select_files.addEventListener("click", async function() {
-  let files = await fileOpen({
-    description: "RawSpoiler.json and TrackerLog.txt",
-    multiple: true
-  });
-
-  raw_spoiler = files.find((f) => f.name == "RawSpoiler.json") || raw_spoiler;
-  if (!raw_spoiler) {
-    alert("selected files did not include RawSpoiler.json");
-  }
-  tracker_log = files.find((f) => f.name == "TrackerLog.txt") || tracker_log;
-  if (!tracker_log) {
-    alert("selected files did not include TrackerLog.txt");
+class Files {
+  static async try_deserialize() {
+    let files = new Files;
+    files.raw_spoiler = await File.try_deserialize("RawSpoiler.json");
+    files.tracker_log = await File.try_deserialize("TrackerLog.txt");
+    return files;
   }
 
-  run_button.disabled = !(raw_spoiler && tracker_log)
-});
+  async open_picker() {
+    let files = await fileOpen({
+      description: "RawSpoiler.json and TrackerLog.txt",
+      multiple: true
+    });
+    await this.raw_spoiler.find_in_list(files);
+    await this.tracker_log.find_in_list(files);
+  }
+}
 
-run_button.addEventListener("click", async function() {
-  if (tracker_log.handle) {
-    if (await tracker_log.handle.requestPermission() == "granted") {
-      let handle = tracker_log.handle;
-      tracker_log = await tracker_log.handle.getFile();
-      tracker_log.handle = tracker_log.hanlde || handle;
+class File {
+  static async try_deserialize(name) {
+    let file = new File;
+    file.name = name;
+    file.handle = await get(name);
+    return file;
+  }
+
+  async find_in_list(list) {
+    let new_file;
+    if (new_file = list.find((f) => f.name == this.name)) {
+      this.file = new_file
+      this.handle = new_file.handle
+      await set(this.name, new_file.handle);
     }
   }
 
-  output.innerHTML = wasm.run(
-    new Uint8Array(await raw_spoiler.arrayBuffer()),
-    new Uint8Array(await tracker_log.arrayBuffer()),
-    show_items_cb.checked,
-    show_unlocked_locations_cb.checked,
-  );
-})
+  async is_available() {
+    if (this.handle) {
+      return this.handle_has_permission();
+    } else {
+      return this.file;
+    }
+  }
+
+  async handle_has_permission() {
+    return this.handle && await this.handle.queryPermission() != "denied"
+  }
+
+  async get_data() {
+    if (this.handle && await this.handle.requestPermission() == "granted") {
+      this.file = await this.handle.getFile();
+    }
+    return new Uint8Array(await this.file.arrayBuffer());
+  }
+
+  async status() {
+    if (await this.handle_has_permission()) {
+      return `${this.name} will be reloaded when changed`;
+    } else if (this.file) {
+      return `${this.name} selected (automatic reloading not detected. Click "Select Files" when the file changes, or use a chromium based browser`;
+    }
+    return `${this.name} has not been selected`;
+  }
+}
+
+async function render_file_status() {
+  let raw_spoiler_available = await files.raw_spoiler.is_available();
+  let tracker_log_available = await files.tracker_log.is_available();
+  raw_spoiler_status.innerHTML = await files.raw_spoiler.status();
+  tracker_log_status.innerHTML = await files.tracker_log.status();
+  run_button.disabled = !(raw_spoiler_available && tracker_log_available);
+}
+
+(async () => {
+  window.files = await Files.try_deserialize();
+  await render_file_status();
+
+  select_files.addEventListener("click", async function() {
+    await files.open_picker();
+    await render_file_status();
+  });
+
+  run_button.addEventListener("click", async function() {
+    output.innerHTML = wasm.run(
+      await files.raw_spoiler.get_data(),
+      await files.tracker_log.get_data(),
+      show_items_cb.checked,
+      show_unlocked_locations_cb.checked,
+    );
+  })
+})();
